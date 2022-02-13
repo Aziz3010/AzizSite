d82cd8686e4b37b48c4cc2508cdbba52efb6f751
unknown <a.abdelazizg@gmail.com>
2022-02-13 19:59:47 +0200

    handle sections display

--- js/main.js
+++ js/main.js
@@ -13,3 +13,42 @@ burgerBTN.addEventListener('click',function(){
 });
 
 // ////////////////////////////////////////////////////////////////
+// add active class when click on links
+let allLinks = document.querySelectorAll('.my-links ul li');
+
+allLinks.forEach((Link)=>{
+    Link.addEventListener("click",function(){
+        
+        allLinks.forEach((otherLink)=>{
+            otherLink.classList.remove('active');
+        })
+
+        Link.classList.add('active');
+
+    })
+})
+
+// ////////////////////////////////////////////////////////////////
+// add display block to the section when click on links
+let allSections = document.querySelectorAll('body section');
+let allLinksHref = document.querySelectorAll('.my-links ul li a');
+
+allLinksHref.forEach((LinkHref)=>{
+    // get href form clicked Ancur
+    LinkHref.addEventListener('click',function(e){
+        let theSectionName = e.target.getAttribute('href');
+        
+        
+        // for each on all sections and remove active class
+        allSections.forEach((Section)=>{
+            Section.style.display = 'none';
+        });
+
+        // add active class to the selected section only
+        let sectionSelected = document.querySelector(`${theSectionName}`);
+        sectionSelected.style.display = 'block';
+    })
+
+})
+
+// ////////////////////////////////////////////////////////////////
