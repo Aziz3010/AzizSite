5f00add8179b3e6ceb6bf08a6bb4385839f535a5
unknown <a.abdelazizg@gmail.com>
2022-02-28 00:53:44 +0200

    add some editting in about section

--- js/main.js
+++ js/main.js
@@ -375,3 +375,21 @@ function inputsEmpty(){
 }
 
 // ////////////////////////////////////////////////////////////////
+// accordion
+const panel_heading_icon = document.querySelectorAll('.panel-heading i');
+const panel_content = document.querySelectorAll('.panel-content');
+
+panel_heading_icon.forEach((icon)=>{
+    icon.addEventListener('click',function(e){
+        let icon = e.target;
+        let parentElement = icon.parentElement;
+        let parentElement_nextElementSibling = parentElement.nextElementSibling
+
+        panel_content.forEach((content)=>{
+            content.classList.remove('active');
+        })
+
+        parentElement_nextElementSibling.classList.add("active");
+    })
+})
+
